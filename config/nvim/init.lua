33741c71c588d57428d96fa65578f2fac9d92c09
David <293+david@users.noreply.github.com>
2024-07-26 20:08:52 +0100

    [nvim] Stop using system clipboard by default

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -40,7 +40,6 @@ vim.opt.autowrite = true
 vim.opt.background = "dark"
 vim.opt.backup = false
 vim.opt.breakindent = true
-vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = { "menu", "menuone", "noinsert", "preview" }
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "both"
