9dbb88250d23e8d156715fdc5f0cbcd8b164223d
David <293+david@users.noreply.github.com>
2024-07-26 20:09:22 +0100

    [nvim] Add session manager

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -112,6 +112,17 @@ require("lazy").setup({
     },
 
     {
+      "Shatur/neovim-session-manager",
+      config = function()
+        local config = require("session_manager.config")
+
+        require("session_manager").setup({
+          autoload_mode = config.AutoloadMode.Disabled,
+        })
+      end,
+    },
+
+    {
       "Wansmer/treesj",
       dependencies = { "nvim-treesitter/nvim-treesitter" },
       keys = {
