7b849859053d00f7d5c74e50651a5082fe1cb8e8
David <293+david@users.noreply.github.com>
2024-07-30 14:19:21 +0100

    [ble.sh] Tweak colors

--- config/blesh/init.sh
+++ config/blesh/init.sh
@@ -9,10 +9,11 @@ ble-face auto_complete="fg=242"
 
 ble-face command_builtin="fg=108"
 ble-face command_builtin_dot="fg=108,bold"
+ble-face command_directory="fg=214,bold"
 ble-face command_function="fg=108"
 
-ble-face command_directory="fg=214,bold"
 ble-face filename_directory="fg=214,bold"
+ble-face filename_orphan="fg=167"
 
 ble-face syntax_error="fg=167,bold"
 
