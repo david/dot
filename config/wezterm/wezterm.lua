92f7e41b8c4dcaea1420356bdf368f374b3c4942
David <293+david@users.noreply.github.com>
2024-08-13 23:12:34 +0100

    [wezterm] Tweak tab bar

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -18,6 +18,7 @@ config.colors = {
 config.font = wt.font("Iosevka Timbuktu")
 config.font_size = 11.0
 config.initial_cols = 119
+config.hide_tab_bar_if_only_one_tab = true
 config.show_new_tab_button_in_tab_bar = false
 config.use_fancy_tab_bar = true
 
