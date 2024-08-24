6bda855f476d664c50331da3bcfd3e948d15e46f
David <293+david@users.noreply.github.com>
2024-08-24 12:49:49 +0100

    Update habitat config

--- lib/blueprints.ex
+++ lib/blueprints.ex
@@ -1,35 +1,46 @@
 defmodule Blueprints do
   def containers do
-    [
-      %{
-        name: "habitat",
-        packages: packages() ++ ["elixir"]
-      },
-      %{
-        name: "sys",
-        packages: packages() ++ ["elixir", "stylua", "lua-language-server"]
-      },
-      %{name: "test"}
-    ]
-    |> Enum.map(&container/1)
+    Enum.map(
+      [
+        %{
+          name: "ar",
+          packages: packages() ++ ["heroku", "opentofu"],
+          programs: [
+            mysql: "8.0",
+            nodejs: [
+              package_manager: :yarn,
+              version: "18"
+            ],
+            ruby: "3.3"
+          ],
+          files: files()
+        },
+        %{
+          name: "habitat",
+          packages: packages() ++ ["elixir"]
+        },
+        %{
+          name: "sys",
+          packages: packages() ++ ["elixir", "stylua", "lua-language-server"]
+        },
+        %{
+          name: "test",
+          programs: [
+            mysql: "8.0"
+          ]
+        }
+      ],
+      &container/1
+    )
   end
 
-  defp container(opts) do
-    Map.merge(
-      opts,
-      %{
-        exports: exports(),
-        features: %{
-          atuin: true,
-          bash: true,
-          zoxide: true
-        },
-        files: files(),
-        os: :archlinux,
-        packages: Map.get(opts, :packages, packages()),
-        root: [System.user_home(), "..", opts.name] |> Path.join() |> Path.expand()
-      }
-    )
+  defp programs do
+    [
+      :atuin,
+      :mise,
+      :starship,
+      :zoxide
+    ]
   end
 
   defp exports do
@@ -40,6 +51,7 @@ defmodule Blueprints do
 
   defp files do
     [
+      {"containers/ar/files/**", "~/.config"},
       {"files/**", "~/.config"},
       {"fonts/*", "~/.local/share/fonts"}
     ]
@@ -47,7 +59,6 @@ defmodule Blueprints do
 
   defp packages do
     [
-      "atuin",
       "base-devel",
       "bat",
       "fd",
@@ -61,11 +72,29 @@ defmodule Blueprints do
       "neovim",
       "noto-fonts-emoji",
       "ripgrep",
-      "starship",
+      "shared-mime-info",
       "ttf-nerd-fonts-symbols-mono",
       "wl-clipboard",
-      "zoxide",
       "wezterm"
     ]
+  end
+
+  defp container(opts) do
+    Map.merge(
+      opts,
+      %{
+        # missing:
+        # elasticsearch via tarball (aur?)
+        # make glibc-locale, man-db, base-devel installable without appearing in :packages
+        # brew install f1bonacc1/tap/process-compose
+        exports: exports(),
+        programs: programs() ++ Map.get(opts, :programs, []),
+        files: Map.get(opts, :files, files()),
+        os: Habitat.OS.ArchLinux,
+        packages: Map.get(opts, :packages, packages()),
+        root: [System.user_home(), "..", opts.name] |> Path.join() |> Path.expand(),
+        shells: [:zsh, :bash]
+      }
+    )
   end
 end
