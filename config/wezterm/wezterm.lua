7378c695bd9e41de4f6e8b897613e992cd2128fc
David <293+david@users.noreply.github.com>
2024-09-27 23:28:46 +0100

    Wezterm changes

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,18 @@
 local wt = require("wezterm")
 local act = wt.action
+
+local function shell(cmd)
+  if cmd then
+    return { "/home/linuxbrew/.linuxbrew/bin/fish", "-c", wt.shell_join_args(cmd) }
+  else
+    return { "/home/linuxbrew/.linuxbrew/bin/fish" }
+  end
+end
+
+local function direnv(cmd)
+  return shell({ "direnv", "exec", ".", table.unpack(cmd) })
+end
+
 local config = {}
 
 config.color_scheme = "GruvboxDarkHard"
@@ -15,36 +28,97 @@ config.colors = {
   },
 }
 
-config.default_prog = { "fish" }
+config.inactive_pane_hsb = {
+  saturation = 0.6,
+  brightness = 0.4,
+}
+
+config.default_prog = shell()
 
 config.freetype_load_target = "HorizontalLcd"
+
 config.font = wt.font("JetBrains Mono")
-config.font_size = 11.5
-config.initial_cols = 126
-config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 11.0
+config.warn_about_missing_glyphs = false
+
+config.initial_cols = 208
 
 config.keys = {
-  { key = "e", mods = "SUPER", action = act.SpawnCommandInNewTab({ args = { "nvim" } }) },
-  { key = "e", mods = "SUPER|CTRL", action = act.SpawnCommandInNewWindow({ args = { "nvim" } }) },
-  { key = "g", mods = "SUPER", action = act.SpawnCommandInNewTab({ args = { "lazygit" } }) },
-  { key = "h", mods = "SUPER", action = act.ActivateTabRelative(-1) },
-  { key = "h", mods = "SUPER|CTRL", action = act.MoveTabRelative(-1) },
-  { key = "l", mods = "SUPER", action = act.ActivateTabRelative(1) },
-  { key = "l", mods = "SUPER|CTRL", action = act.MoveTabRelative(1) },
-  { key = "s", mods = "SUPER", action = act.SpawnTab("CurrentPaneDomain") },
-  { key = "s", mods = "SUPER|CTRL", action = act.SpawnWindow },
-  { key = 'u', mods = 'SUPER|CTRL', action = wt.action.CharSelect {
-    copy_on_select = true,
-    copy_to = 'ClipboardAndPrimarySelection',
-  }},
+  {
+    key = "e",
+    mods = "SUPER",
+    action = act.SplitPane({
+      direction = "Right",
+      command = { args = direnv({ "nvim" }) },
+      size = { Percent = 57 },
+    }),
+  },
+  { key = "g", mods = "SUPER", action = act.SpawnCommandInNewTab({
+    args = shell({ "lazygit" }),
+  }) },
+
+  { key = "h", mods = "SUPER", action = act.ActivatePaneDirection("Left") },
+  { key = "j", mods = "SUPER", action = act.ActivatePaneDirection("Down") },
+  { key = "k", mods = "SUPER", action = act.ActivatePaneDirection("Up") },
+  { key = "l", mods = "SUPER", action = act.ActivatePaneDirection("Right") },
+
+  { key = "q", mods = "SUPER", action = act.CloseCurrentPane({ confirm = false }) },
+
+  { key = ",", mods = "SUPER", action = act.ActivateTabRelative(-1) },
+  { key = ",", mods = "SUPER|CTRL", action = act.MoveTabRelative(-1) },
+  { key = ".", mods = "SUPER", action = act.ActivateTabRelative(1) },
+  { key = ".", mods = "SUPER|CTRL", action = act.MoveTabRelative(1) },
+  {
+    key = "s",
+    mods = "SUPER",
+    action = act.SplitPane({
+      direction = "Left",
+      command = { args = shell() },
+      size = { Percent = 43 },
+    }),
+  },
+  {
+    key = "u",
+    mods = "SUPER|CTRL",
+    action = wt.action.CharSelect({
+      copy_on_select = true,
+      copy_to = "ClipboardAndPrimarySelection",
+    }),
+  },
+
+  {
+    key = "x",
+    mods = "CTRL|SHIFT",
+    action = act.PromptInputLine({
+      description = wt.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Enter name for new workspace" },
+      }),
+      action = wt.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:perform_action(
+            act.SwitchToWorkspace({
+              name = line,
+            }),
+            pane
+          )
+        end
+      end),
+    }),
+  },
 }
 
 config.line_height = 1.05
 
 config.show_new_tab_button_in_tab_bar = false
-config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = true
+config.tab_max_width = 64
+config.use_fancy_tab_bar = false
 
-config.warn_about_missing_glyphs = false
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
 
@@ -55,6 +129,6 @@ config.window_frame = {
   font_size = 10.0,
 }
 
-config.window_padding = { top = 8, right = 8, bottom = 8, left = 8 }
+config.window_padding = { top = 8, right = 8, bottom = 8, left = 12 }
 
 return config
