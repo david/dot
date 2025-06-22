b210ecb4fa4c551e886c35e04ec17034550f819d
David <293+david@users.noreply.github.com>
2025-06-22 16:33:17 +0100

    [nvim] Recover "treesj" setup

--- roles/common-home/files/config/nvim/init.lua
+++ roles/common-home/files/config/nvim/init.lua
@@ -361,7 +361,9 @@ require("snacks").setup({
 require("supermaven-nvim").setup({})
 require("tiny-glimmer").setup({})
 require("todo-comments").setup({})
-require("treesj").setup({})
+require("treesj").setup({
+  use_default_keymaps = false,
+})
 require("virt-column").setup({})
 require("which-key").setup({})
 
