9508dfe0684aacc37ce8c4a8158b30783f2db86c
David <293+david@users.noreply.github.com>
2025-06-22 16:32:57 +0100

    [nvim] Recover "quicker" setup

--- roles/common-home/files/config/nvim/init.lua
+++ roles/common-home/files/config/nvim/init.lua
@@ -315,8 +315,25 @@ require("nvim-treesitter.configs").setup({
 
 require("nvim-ts-autotag").setup({})
 
+require("quicker").setup({
+  keys = {
+    {
+      ">",
+      function()
+        require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+      end,
+      desc = "Expand quickfix context",
+    },
+    {
+      "<",
+      function()
+        require("quicker").collapse()
+      end,
+      desc = "Collapse quickfix context",
+    },
+  },
+})
 
-require("quicker").setup({})
 require("rainbow-delimiters.setup").setup({})
 require("snacks").setup({})
 require("supermaven-nvim").setup({})
