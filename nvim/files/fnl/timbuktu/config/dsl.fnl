438eedd2f54613da11463694c1299bebe87b0f14
David Leal <293+david@users.noreply.github.com>
2025-08-08 11:34:17 +0100

    Clean up dsl.fnl structure a bit

--- nvim/files/fnl/timbuktu/config/dsl.fnl
+++ nvim/files/fnl/timbuktu/config/dsl.fnl
@@ -1,3 +1,23 @@
+(local handlers {})
+
+(lambda define-handler-group [name]
+  (set (. handlers name) {}))
+
+(lambda ensure-handler-group [name]
+  (assert (. handlers name) (.. "no handler group: " name)))
+
+(lambda define-handler [group handler-name config]
+  (ensure-handler-group group)
+  (assert (. config :fn) (.. "no handler function: " handler-name))
+  (set (. handlers group handler-name) config))
+
+(lambda call-handlers [group-name & rest]
+  (ensure-handler-group group-name)
+  (each [key handler (pairs (. handlers group-name))]
+    (handler.fn (unpack rest))))
+
+;;;; reusable handlers
+
 (lambda keymap-handler [{:keymap ?config}]
   (when ?config
     (each [key val (pairs ?config)]
@@ -5,61 +25,78 @@
         {: cmd : mode} (vim.keymap.set mode key cmd)
         cmd (vim.keymap.set :n key cmd)))))
 
-(local vim-handlers {:colorscheme {}
-                     :g {}
-                     :opt {}
-                     :keymap {:fn keymap-handler}})
+;;;; group
 
-(lambda vim-handlers.colorscheme.fn [{:colorscheme ?config}]
-  (case ?config
-    (where str (= (type str) :string)) (do
-                                         ((. (require str) :setup) {})
-                                         (vim.cmd.colorscheme str))))
+(define-handler-group :nvim)
 
-(lambda vim-handlers.g.fn [{:g ?config}]
-  (each [key val (pairs ?config)]
-    (set (. vim.g key) val)))
+(define-handler :nvim
+  :colorscheme
+  {:fn (lambda [{:colorscheme ?config}]
+         (case ?config
+           (where str (= (type str) :string)) (do
+                                                ((. (require str) :setup) {})
+                                                (vim.cmd.colorscheme str))))})
 
-(lambda vim-handlers.opt.fn [{:opt ?config}]
-  (each [key val (pairs ?config)]
-    (set (. vim.opt key) val)))
+(define-handler :nvim
+  :g
+  {:fn (lambda [{:g ?config}]
+         (each [key val (pairs ?config)]
+           (set (. vim.g key) val)))})
 
-(local plugin-handlers {:opt {:fn (lambda [{:opt ?opts} plugin]
-                                    (case (. (require plugin) :setup)
-                                      setup (setup (or ?opts {}))))}
-                        :keymap {:fn keymap-handler}})
+(define-handler :nvim
+  :opt
+  {:fn (lambda [{:opt ?config}]
+         (each [key val (pairs ?config)]
+           (set (. vim.opt key) val)))})
 
-(local ft-handlers {:lang {} :plugin {}})
+(define-handler :nvim :keymap {:fn keymap-handler})
 
-(lambda ft-handlers.plugin.fn [{:plugin ?config} ft]
-  (let [group-name (.. ft :-ft-plugin)
-        group (vim.api.nvim_create_augroup group-name {:clear true})]
-    (each [key val (pairs (or ?config {}))]
-      (vim.api.nvim_create_autocmd :FileType
-                                   {:pattern ft
-                                    :callback #((. (require key) :setup) val)
-                                    : group}))))
+;;;; group
 
-(lambda ft-handlers.lang.fn [_config ft]
-  (let [group (vim.api.nvim_create_augroup (.. ft :-ft-lang) {:clear true})]
-    (vim.api.nvim_create_autocmd :FileType
-                                 {:pattern ft
-                                  :callback #(vim.treesitter.start)
-                                  : group})))
+(define-handler-group :plugin)
 
-(local handlers {:filetype ft-handlers
-                 :nvim vim-handlers
-                 :plugin plugin-handlers})
+(define-handler :plugin
+  :opt
+  {:fn (lambda [{:opt ?opts} plugin]
+         (case (. (require plugin) :setup)
+           setup (setup (or ?opts {}))))})
 
-(lambda call-handlers [handlers & rest]
-  (each [key handler (pairs handlers)]
-    (handler.fn (unpack rest))))
+(define-handler :plugin :keymap {:fn keymap-handler})
+
+;;;; group
+
+(define-handler-group :filetype)
+
+(define-handler :filetype
+  :plugin
+  {:fn (lambda [{:plugin ?config} ft]
+         (let [group-name (.. ft :-ft-plugin)
+               group (vim.api.nvim_create_augroup group-name {:clear true})]
+           (each [key val (pairs (or ?config {}))]
+             (vim.api.nvim_create_autocmd :FileType
+                                          {:pattern ft
+                                           :callback #((. (require key) :setup) val)
+                                           : group}))))})
+
+(define-handler :filetype
+  :lang
+  {:fn (lambda [_config ft]
+         (let [group (vim.api.nvim_create_augroup (.. ft :-ft-lang)
+                                                  {:clear true})]
+           (vim.api.nvim_create_autocmd :FileType
+                                        {:pattern ft
+                                         :callback #(vim.treesitter.start)
+                                         : group})))})
+
+;;;; setup
 
 (lambda setup [name ?config]
   (let [config (or ?config {})]
     (case name
-      :nvim (call-handlers handlers.nvim config)
-      [:filetype ft] (call-handlers handlers.filetype config ft)
-      _ (call-handlers handlers.plugin config name))))
+      :nvim (call-handlers :nvim config)
+      [:filetype ft] (call-handlers :filetype config ft)
+      _ (call-handlers :plugin config name))))
+
+;; exports
 
 {: setup}
--- nvim/files/lua/timbuktu/config/dsl.lua
+++ nvim/files/lua/timbuktu/config/dsl.lua
@@ -1,4 +1,33 @@
 -- [nfnl] fnl/timbuktu/config/dsl.fnl
+local handlers = {}
+local function define_handler_group(name)
+  _G.assert((nil ~= name), "Missing argument name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:3")
+  handlers[name] = {}
+  return nil
+end
+local function ensure_handler_group(name)
+  _G.assert((nil ~= name), "Missing argument name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:6")
+  return assert(handlers[name], ("no handler group: " .. name))
+end
+local function define_handler(group, handler_name, config)
+  _G.assert((nil ~= config), "Missing argument config on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:9")
+  _G.assert((nil ~= handler_name), "Missing argument handler-name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:9")
+  _G.assert((nil ~= group), "Missing argument group on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:9")
+  ensure_handler_group(group)
+  assert(config.fn, ("no handler function: " .. handler_name))
+  handlers[group][handler_name] = config
+  return nil
+end
+local function call_handlers(group_name, ...)
+  local rest = {...}
+  _G.assert((nil ~= rest), "Missing argument rest on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:14")
+  _G.assert((nil ~= group_name), "Missing argument group-name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:14")
+  ensure_handler_group(group_name)
+  for key, handler in pairs(handlers[group_name]) do
+    handler.fn(unpack(rest))
+  end
+  return nil
+end
 local function keymap_handler(_1_)
   local _3fconfig = _1_["keymap"]
   if _3fconfig then
@@ -18,15 +47,15 @@ local function keymap_handler(_1_)
     return nil
   end
 end
-local vim_handlers = {colorscheme = {}, g = {}, opt = {}, keymap = {fn = keymap_handler}}
-vim_handlers.colorscheme.fn = function(_4_)
+define_handler_group("nvim")
+local function _5_(_4_)
   local _3fconfig = _4_["colorscheme"]
-  local and_5_ = (nil ~= _3fconfig)
-  if and_5_ then
+  local and_6_ = (nil ~= _3fconfig)
+  if and_6_ then
     local str = _3fconfig
-    and_5_ = (type(str) == "string")
+    and_6_ = (type(str) == "string")
   end
-  if and_5_ then
+  if and_6_ then
     local str = _3fconfig
     require(str).setup({})
     return vim.cmd.colorscheme(str)
@@ -34,76 +63,73 @@ vim_handlers.colorscheme.fn = function(_4_)
     return nil
   end
 end
-vim_handlers.g.fn = function(_8_)
-  local _3fconfig = _8_["g"]
+define_handler("nvim", "colorscheme", {fn = _5_})
+local function _10_(_9_)
+  local _3fconfig = _9_["g"]
   for key, val in pairs(_3fconfig) do
     vim.g[key] = val
   end
   return nil
 end
-vim_handlers.opt.fn = function(_9_)
-  local _3fconfig = _9_["opt"]
+define_handler("nvim", "g", {fn = _10_})
+local function _12_(_11_)
+  local _3fconfig = _11_["opt"]
   for key, val in pairs(_3fconfig) do
     vim.opt[key] = val
   end
   return nil
 end
-local plugin_handlers
-local function _11_(_10_, plugin)
-  local _3fopts = _10_["opt"]
-  _G.assert((nil ~= plugin), "Missing argument plugin on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:27")
-  local _12_ = require(plugin).setup
-  if (nil ~= _12_) then
-    local setup = _12_
+define_handler("nvim", "opt", {fn = _12_})
+define_handler("nvim", "keymap", {fn = keymap_handler})
+define_handler_group("plugin")
+local function _14_(_13_, plugin)
+  local _3fopts = _13_["opt"]
+  _G.assert((nil ~= plugin), "Missing argument plugin on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:60")
+  local _15_ = require(plugin).setup
+  if (nil ~= _15_) then
+    local setup = _15_
     return setup((_3fopts or {}))
   else
     return nil
   end
 end
-plugin_handlers = {opt = {fn = _11_}, keymap = {fn = keymap_handler}}
-local ft_handlers = {lang = {}, plugin = {}}
-ft_handlers.plugin.fn = function(_14_, ft)
-  local _3fconfig = _14_["plugin"]
-  _G.assert((nil ~= ft), "Missing argument ft on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:34")
+define_handler("plugin", "opt", {fn = _14_})
+define_handler("plugin", "keymap", {fn = keymap_handler})
+define_handler_group("filetype")
+local function _18_(_17_, ft)
+  local _3fconfig = _17_["plugin"]
+  _G.assert((nil ~= ft), "Missing argument ft on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:72")
   local group_name = (ft .. "-ft-plugin")
   local group = vim.api.nvim_create_augroup(group_name, {clear = true})
   for key, val in pairs((_3fconfig or {})) do
-    local function _15_()
+    local function _19_()
       return require(key).setup(val)
     end
-    vim.api.nvim_create_autocmd("FileType", {pattern = ft, callback = _15_, group = group})
+    vim.api.nvim_create_autocmd("FileType", {pattern = ft, callback = _19_, group = group})
   end
   return nil
 end
-ft_handlers.lang.fn = function(_config, ft)
-  _G.assert((nil ~= ft), "Missing argument ft on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:43")
+define_handler("filetype", "plugin", {fn = _18_})
+local function _20_(_config, ft)
+  _G.assert((nil ~= ft), "Missing argument ft on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:83")
   local group = vim.api.nvim_create_augroup((ft .. "-ft-lang"), {clear = true})
-  local function _16_()
+  local function _21_()
     return vim.treesitter.start()
   end
-  return vim.api.nvim_create_autocmd("FileType", {pattern = ft, callback = _16_, group = group})
+  return vim.api.nvim_create_autocmd("FileType", {pattern = ft, callback = _21_, group = group})
 end
-local handlers = {filetype = ft_handlers, nvim = vim_handlers, plugin = plugin_handlers}
-local function call_handlers(handlers0, ...)
-  local rest = {...}
-  _G.assert((nil ~= rest), "Missing argument rest on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:54")
-  _G.assert((nil ~= handlers0), "Missing argument handlers on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:54")
-  for key, handler in pairs(handlers0) do
-    handler.fn(unpack(rest))
-  end
-  return nil
-end
+define_handler("filetype", "lang", {fn = _20_})
 local function setup(name, _3fconfig)
-  _G.assert((nil ~= name), "Missing argument name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:58")
+  _G.assert((nil ~= name), "Missing argument name on /var/home/david/Worktrees/dot/nvim/files/fnl/timbuktu/config/dsl.fnl:93")
   local config = (_3fconfig or {})
   if (name == "nvim") then
-    return call_handlers(handlers.nvim, config)
+    return call_handlers("nvim", config)
   elseif ((_G.type(name) == "table") and (name[1] == "filetype") and (nil ~= name[2])) then
     local ft = name[2]
-    return call_handlers(handlers.filetype, config, ft)
+    return call_handlers("filetype", config, ft)
   else
     local _ = name
-    return call_handlers(handlers.plugin, config, name)
+    return call_handlers("plugin", config, name)
   end
 end
 return {setup = setup}
