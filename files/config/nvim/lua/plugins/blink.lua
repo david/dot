13a443a2ff2279a08f02f58e094b37095f0c63ee
David Leal <293+david@users.noreply.github.com>
2025-08-12 13:06:59 +0100

    [nvim] Add blink

---
+++ files/config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+  opts = {
+    sources = {
+      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+      },
+    },
+  },
+}
