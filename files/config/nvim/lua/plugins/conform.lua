9c6a397bf4ef29ddf8c7cc732e8909d1905145c3
David Leal <293+david@users.noreply.github.com>
2025-08-17 15:55:38 +0100

    [nvim] Add biome to conform formatters

--- files/config/nvim/lua/plugins/conform.lua
+++ files/config/nvim/lua/plugins/conform.lua
@@ -2,6 +2,8 @@ return {
   "stevearc/conform.nvim",
   opts = {
     formatters_by_ft = {
+      javascript = { "biome" },
+      json = { "biome" },
       lua = { "stylua" },
       ["*"] = { "trim_whitespace" },
     },
