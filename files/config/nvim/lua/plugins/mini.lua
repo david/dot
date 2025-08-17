462092e57dc1827525e7c045236a1121dccb2850
David Leal <293+david@users.noreply.github.com>
2025-08-17 16:05:32 +0100

    [nvim] Enable more mini plugins

--- files/config/nvim/lua/plugins/mini.lua
+++ files/config/nvim/lua/plugins/mini.lua
@@ -1,6 +1,15 @@
 return {
   "echasnovski/mini.nvim",
   config = function()
+    require("mini.ai").setup({})
+    require("mini.git").setup({})
+    require("mini.icons").setup({})
+    require("mini.move").setup({
+      mappings = {
+        left = "<",
+        right = ">",
+      },
+    })
     require("mini.operators").setup({})
   end,
 }
