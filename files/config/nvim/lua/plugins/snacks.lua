8deeae941cf2052fd3fc9db095eb5a434005fb1b
David Leal <293+david@users.noreply.github.com>
2025-08-17 15:59:57 +0100

    [nvim] Change snacks dashboard autokeys

--- files/config/nvim/lua/plugins/snacks.lua
+++ files/config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
   opts = {
     dashboard = {
       width = 82,
+      autokeys = "asdfghjkl;",
       preset = {
         header = [[
 ░██████████░██                ░██                   ░██          ░██
