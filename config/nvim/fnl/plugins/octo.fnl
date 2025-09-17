db832e4eb7f87a168e53b55000bd30f6832777fe
David Leal <293+david@users.noreply.github.com>
2025-09-17 11:49:57 +0100

    [nvim] Add shortcut to create new issue

--- config/nvim/fnl/plugins/octo.fnl
+++ config/nvim/fnl/plugins/octo.fnl
@@ -1,6 +1,7 @@
 (setup :pwntester/octo.nvim
        {:opts {:picker :snacks :default_to_projects_v2 true}
-        :keys [(kv :<leader>fi "<cmd>Octo issue list<cr>" {:desc :Issues})]
+        :keys [(kv :<leader>fi "<cmd>Octo issue list<cr>" {:desc :Issues})
+               (kv :<leader>ni "<cmd>Octo issue create<cr>" {:desc :Issue})]
         :dependencies [:nvim-lua/plenary.nvim
                        :folke/snacks.nvim
                        :nvim-tree/nvim-web-devicons]})
--- config/nvim/lua/plugins/octo.lua
+++ config/nvim/lua/plugins/octo.lua
@@ -1,2 +1,2 @@
--- [nfnl] config/nvim/fnl/plugins/octo.fnl
-return setup("pwntester/octo.nvim", {opts = {picker = "snacks", default_to_projects_v2 = true}, keys = {kv("<leader>fi", "<cmd>Octo issue list<cr>", {desc = "Issues"})}, dependencies = {"nvim-lua/plenary.nvim", "folke/snacks.nvim", "nvim-tree/nvim-web-devicons"}})
+-- [nfnl] fnl/plugins/octo.fnl
+return setup("pwntester/octo.nvim", {opts = {picker = "snacks", default_to_projects_v2 = true}, keys = {kv("<leader>fi", "<cmd>Octo issue list<cr>", {desc = "Issues"}), kv("<leader>ni", "<cmd>Octo issue create<cr>", {desc = "Issue"})}, dependencies = {"nvim-lua/plenary.nvim", "folke/snacks.nvim", "nvim-tree/nvim-web-devicons"}})
