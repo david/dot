87e8b0630c27c4df2dfebfd51bd7a80cc883d53a
David Leal <293+david@users.noreply.github.com>
2025-10-29 21:05:37 +0000

    [nvim] Use terminal in overseer when running a task

--- config/nvim/lua/plugins/overseer.lua
+++ config/nvim/lua/plugins/overseer.lua
@@ -1,2 +1,27 @@
 -- [nfnl] fnl/plugins/overseer.fnl
-return setup("stevearc/overseer.nvim", {opts = {}, keys = {kv("<leader>ro", "<cmd>OverseerRun<cr>", {desc = "Overseer"}), kv("<D-r>", "<cmd>OverseerRun<cr>"), kv("<leader>oo", "<cmd>OverseerOpen<cr>", {desc = "Overseer"})}})
+return {
+  "stevearc/overseer.nvim",
+  opts = {
+    component_aliases = {
+      -- Most tasks are initialized with the default components
+      default = {
+        { "open_output", direction = "float", focus = true },
+        { "display_duration", detail_level = 2 },
+        "on_output_summarize",
+        "on_exit_set_status",
+        "on_complete_notify",
+        { "on_complete_dispose", require_view = { "SUCCESS", "FAILURE" } },
+      },
+    },
+    strategy = "terminal",
+    task_win = {
+      border = "none",
+      padding = 3,
+    },
+  },
+  keys = {
+    { "<leader>ro", "<cmd>OverseerRun<cr>", desc = "Overseer" },
+    { "<D-r>", "<cmd>OverseerRun<cr>" },
+    { "<leader>oo", "<cmd>OverseerOpen<cr>", desc = "Overseer" },
+  },
+}
