7496d3d0e4d363e1fa6108f3332fbfee76b174fd
David Leal <293+david@users.noreply.github.com>
2025-10-31 17:25:09 +0000

    [nvim] Don't remember what I did here

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,57 @@
--- [nfnl] config/nvim/fnl/plugins/lspconfig.fnl
-local function _1_()
-  local function _2_(diagnostic)
-    local diagnostic_message = {[vim.diagnostic.severity.ERROR] = diagnostic.message, [vim.diagnostic.severity.WARN] = diagnostic.message, [vim.diagnostic.severity.INFO] = diagnostic.message, [vim.diagnostic.severity.HINT] = diagnostic.message}
-    return diagnostic_message[diagnostic.severity]
-  end
-  return vim.diagnostic.config({severity_sort = true, float = {border = "rounded", source = "if_many"}, underline = {severity = vim.diagnostic.severity.ERROR}, signs = {text = {[vim.diagnostic.severity.ERROR] = "\243\176\133\154 ", [vim.diagnostic.severity.WARN] = "\243\176\128\170 ", [vim.diagnostic.severity.INFO] = "\243\176\139\189 ", [vim.diagnostic.severity.HINT] = "\243\176\140\182 "}}, virtual_text = {source = "if_many", spacing = 2, format = _2_}})
-end
-return setup("neovim/nvim-lspconfig", {config = _1_, dependencies = {kv("mason-org/mason.nvim", {opts = {}}), kv("mason-org/mason-lspconfig.nvim", {opts = {ensure_installed = {"eslint", "expert", "herb_ls", "lua_ls", "ruby_lsp", "tailwindcss", "ts_ls"}}}), kv("WhoIsSethDaniel/mason-tool-installer.nvim", {opts = {ensure_installed = {"erb-formatter", "stylua"}}}), kv("j-hui/fidget.nvim", {opts = {}}), "saghen/blink.cmp"}})
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    vim.diagnostic.config({
+      severity_sort = true,
+      float = { border = "rounded", source = "if_many" },
+      underline = { severity = vim.diagnostic.severity.ERROR },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.INFO] = "󰋽 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+      },
+    })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+
+        map("ga", vim.lsp.buf.code_action, "Code Action", { "n", "x" })
+      end,
+    })
+  end,
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    {
+      "mason-org/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = {
+          "eslint",
+          "expert",
+          "herb_ls",
+          "lua_ls",
+          "ruby_lsp",
+          "tailwindcss",
+          "ts_ls",
+        },
+      },
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      opts = {
+        ensure_installed = {
+          "erb-formatter",
+          "stylua",
+        },
+      },
+    },
+    "saghen/blink.cmp",
+  },
+}
